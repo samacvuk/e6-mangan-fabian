c624f4ef5ef137c028dea1be2b254f333d16a376
samacvuk <fabian.lorenzo@acad.pucrs.br>
2016-10-26 12:55:51 -0200

    Final Commit, class working and ready to release.

--- e6/src/treeTutorial/Tree.java
+++ e6/src/treeTutorial/Tree.java
@@ -1,4 +1,5 @@
 package treeTutorial;
+
 import javax.swing.JEditorPane;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -17,93 +18,181 @@ import java.io.IOException;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 
-public class Tree extends JPanel implements TreeSelectionListener{
+public class Tree extends JPanel implements TreeSelectionListener {
 	private JEditorPane htmlPane;
 	private JTree tree;
-    private URL helpURL;
-    private static boolean DEBUG = false;
-    private static boolean playWithLineStyle = false;
-    private static String lineStyle = "Horizontal";
-    private static boolean useSystemLookAndFeel = false;
-    
-    public Tree() {
-        super(new GridLayout(1,0));
-        DefaultMutableTreeNode top =
-                new DefaultMutableTreeNode("The Java Series");
-            createNodes(top);
-            tree = new JTree(top);
-            tree.getSelectionModel().setSelectionMode
-                    (TreeSelectionModel.SINGLE_TREE_SELECTION);
-    }
-	
+	private URL helpURL;
+	private static boolean DEBUG = false;
+	private static boolean playWithLineStyle = false;
+	private static String lineStyle = "Horizontal";
+	private static boolean useSystemLookAndFeel = false;
+
+	public Tree() {
+		super(new GridLayout(1, 0));
+		DefaultMutableTreeNode top = new DefaultMutableTreeNode("The Java Series");
+		createNodes(top);
+		tree = new JTree(top);
+		tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+		tree.addTreeSelectionListener(this);
+
+		if (playWithLineStyle) {
+			System.out.println("line style = " + lineStyle);
+			tree.putClientProperty("JTree.lineStyle", lineStyle);
+		}
+
+		JScrollPane treeView = new JScrollPane(tree);
+
+		htmlPane = new JEditorPane();
+		htmlPane.setEditable(false);
+		initHelp();
+		JScrollPane htmlView = new JScrollPane(htmlPane);
+
+		JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+		splitPane.setTopComponent(treeView);
+		splitPane.setBottomComponent(htmlView);
+
+		Dimension minimumSize = new Dimension(100, 50);
+		htmlView.setMinimumSize(minimumSize);
+		treeView.setMinimumSize(minimumSize);
+		splitPane.setDividerLocation(100);
+		splitPane.setPreferredSize(new Dimension(500, 300));
+
+		add(splitPane);
+	}
+
+	private void initHelp() {
+		String s = "TreeDemoHelp.html";
+		helpURL = getClass().getResource(s);
+		if (helpURL == null) {
+			System.err.println("Couldn't open help file: " + s);
+		} else if (DEBUG) {
+			System.out.println("Help URL is " + helpURL);
+		}
+
+		displayURL(helpURL);
+	}
+
+	private void displayURL(URL url) {
+		try {
+			if (url != null) {
+				htmlPane.setPage(url);
+			} else { // null url
+				htmlPane.setText("File Not Found");
+				if (DEBUG) {
+					System.out.println("Attempted to display a null URL.");
+				}
+			}
+		} catch (IOException e) {
+			System.err.println("Attempted to read a bad URL: " + url);
+		}
+	}
+
 	@Override
 	public void valueChanged(TreeSelectionEvent e) {
-		// TODO Auto-generated method stub
-		
+		DefaultMutableTreeNode node = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
+
+		if (node == null)
+			return;
+
+		Object nodeInfo = node.getUserObject();
+		if (node.isLeaf()) {
+			BookInfo book = (BookInfo) nodeInfo;
+			displayURL(book.bookURL);
+			if (DEBUG) {
+				System.out.print(book.bookURL + ":  \n    ");
+			}
+		} else {
+			displayURL(helpURL);
+		}
+		if (DEBUG) {
+			System.out.println(nodeInfo.toString());
+		}
+
 	}
-	
+
 	private void createNodes(DefaultMutableTreeNode top) {
-        DefaultMutableTreeNode category = null;
-        DefaultMutableTreeNode book = null;
+		DefaultMutableTreeNode category = null;
+		DefaultMutableTreeNode book = null;
 
-        category = new DefaultMutableTreeNode("Books for Java Programmers");
-        top.add(category);
-  
-        book = new DefaultMutableTreeNode(new BookInfo
-            ("The Java Tutorial: A Short Course on the Basics",
-            "tutorial.html"));
-        category.add(book);
-    
-        book = new DefaultMutableTreeNode(new BookInfo
-            ("The Java Tutorial Continued: The Rest of the JDK",
-            "tutorialcont.html"));
-        category.add(book);
-     
-        book = new DefaultMutableTreeNode(new BookInfo
-            ("The JFC Swing Tutorial: A Guide to Constructing GUIs",
-            "swingtutorial.html"));
-        category.add(book);
-  
-        book = new DefaultMutableTreeNode(new BookInfo
-            ("Effective Java Programming Language Guide",
-	     "bloch.html"));
-        category.add(book);
-    
-        book = new DefaultMutableTreeNode(new BookInfo
-            ("The Java Programming Language", "arnold.html"));
-        category.add(book);
-    
-        book = new DefaultMutableTreeNode(new BookInfo
-            ("The Java Developers Almanac",
-             "chan.html"));
-        category.add(book);
+		category = new DefaultMutableTreeNode("Books for Java Programmers");
+		top.add(category);
 
-        category = new DefaultMutableTreeNode("Books for Java Implementers");
-        top.add(category);
-     
-        book = new DefaultMutableTreeNode(new BookInfo
-            ("The Java Virtual Machine Specification",
-             "vm.html"));
-        category.add(book);
-        
-        book = new DefaultMutableTreeNode(new BookInfo
-            ("The Java Language Specification",
-             "jls.html"));
-        category.add(book);
-    }
-	
+		book = new DefaultMutableTreeNode(
+				new BookInfo("The Java Tutorial: A Short Course on the Basics", "tutorial.html"));
+		category.add(book);
+
+		book = new DefaultMutableTreeNode(
+				new BookInfo("The Java Tutorial Continued: The Rest of the JDK", "tutorialcont.html"));
+		category.add(book);
+
+		book = new DefaultMutableTreeNode(
+				new BookInfo("The JFC Swing Tutorial: A Guide to Constructing GUIs", "swingtutorial.html"));
+		category.add(book);
+
+		book = new DefaultMutableTreeNode(new BookInfo("Effective Java Programming Language Guide", "bloch.html"));
+		category.add(book);
+
+		book = new DefaultMutableTreeNode(new BookInfo("The Java Programming Language", "arnold.html"));
+		category.add(book);
+
+		book = new DefaultMutableTreeNode(new BookInfo("The Java Developers Almanac", "chan.html"));
+		category.add(book);
+
+		category = new DefaultMutableTreeNode("Books for Java Implementers");
+		top.add(category);
+
+		book = new DefaultMutableTreeNode(new BookInfo("The Java Virtual Machine Specification", "vm.html"));
+		category.add(book);
+
+		book = new DefaultMutableTreeNode(new BookInfo("The Java Language Specification", "jls.html"));
+		category.add(book);
+	}
+
 	private class BookInfo {
-        public String bookName;
-        public URL bookURL;
+		public String bookName;
+		public URL bookURL;
 
-        public BookInfo(String book, String filename) {
-            bookName = book;
-            bookURL = getClass().getResource(filename);
-            if (bookURL == null) {
-                System.err.println("Couldn't find file: "
-                                   + filename);
-            }
-        }
+		public BookInfo(String book, String filename) {
+			bookName = book;
+			bookURL = getClass().getResource(filename);
+			if (bookURL == null) {
+				System.err.println("Couldn't find file: " + filename);
+			}
+		}
 
+		public String toString() {
+			return bookName;
+		}
+	}
+
+	private static void createAndShowGUI() {
+		if (useSystemLookAndFeel) {
+			try {
+				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			} catch (Exception e) {
+				System.err.println("Couldn't use system look and feel.");
+			}
+		}
+
+		// Create and set up the window.
+		JFrame frame = new JFrame("TreeDemo");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// Add content to the window.
+		frame.add(new Tree());
+
+		// Display the window.
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		// Schedule a job for the event dispatch thread:
+		// creating and showing this application's GUI.
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
 	}
 }
