62567655a426652236d5407c091aa4ed583176e5
samacvuk <fabian.lorenzo@acad.pucrs.br>
2016-10-26 12:40:23 -0200

    Commiting initial files and software model through imports.

---
+++ e6/src/treeTutorial/Tree.java
@@ -0,0 +1,29 @@
+package treeTutorial;
+import javax.swing.JEditorPane;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.UIManager;
+
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeSelectionModel;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+
+import java.net.URL;
+import java.io.IOException;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+public class Tree extends JPanel implements TreeSelectionListener{
+	private JEditorPane htmlPane;
+	
+	@Override
+	public void valueChanged(TreeSelectionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
