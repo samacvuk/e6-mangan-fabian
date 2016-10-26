a6238e1ba0fffc27e68eed69279ac9a2307348bf
samacvuk <fabian.lorenzo@acad.pucrs.br>
2016-10-26 12:46:23 -0200

    Implemented Initial Underlying Functionality of the Panel.

--- e6/src/treeTutorial/Tree.java
+++ e6/src/treeTutorial/Tree.java
@@ -19,11 +19,91 @@ import java.awt.GridLayout;
 
 public class Tree extends JPanel implements TreeSelectionListener{
 	private JEditorPane htmlPane;
+	private JTree tree;
+    private URL helpURL;
+    private static boolean DEBUG = false;
+    private static boolean playWithLineStyle = false;
+    private static String lineStyle = "Horizontal";
+    private static boolean useSystemLookAndFeel = false;
+    
+    public Tree() {
+        super(new GridLayout(1,0));
+        DefaultMutableTreeNode top =
+                new DefaultMutableTreeNode("The Java Series");
+            createNodes(top);
+            tree = new JTree(top);
+            tree.getSelectionModel().setSelectionMode
+                    (TreeSelectionModel.SINGLE_TREE_SELECTION);
+    }
 	
 	@Override
 	public void valueChanged(TreeSelectionEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	private void createNodes(DefaultMutableTreeNode top) {
+        DefaultMutableTreeNode category = null;
+        DefaultMutableTreeNode book = null;
 
+        category = new DefaultMutableTreeNode("Books for Java Programmers");
+        top.add(category);
+  
+        book = new DefaultMutableTreeNode(new BookInfo
+            ("The Java Tutorial: A Short Course on the Basics",
+            "tutorial.html"));
+        category.add(book);
+    
+        book = new DefaultMutableTreeNode(new BookInfo
+            ("The Java Tutorial Continued: The Rest of the JDK",
+            "tutorialcont.html"));
+        category.add(book);
+     
+        book = new DefaultMutableTreeNode(new BookInfo
+            ("The JFC Swing Tutorial: A Guide to Constructing GUIs",
+            "swingtutorial.html"));
+        category.add(book);
+  
+        book = new DefaultMutableTreeNode(new BookInfo
+            ("Effective Java Programming Language Guide",
+	     "bloch.html"));
+        category.add(book);
+    
+        book = new DefaultMutableTreeNode(new BookInfo
+            ("The Java Programming Language", "arnold.html"));
+        category.add(book);
+    
+        book = new DefaultMutableTreeNode(new BookInfo
+            ("The Java Developers Almanac",
+             "chan.html"));
+        category.add(book);
+
+        category = new DefaultMutableTreeNode("Books for Java Implementers");
+        top.add(category);
+     
+        book = new DefaultMutableTreeNode(new BookInfo
+            ("The Java Virtual Machine Specification",
+             "vm.html"));
+        category.add(book);
+        
+        book = new DefaultMutableTreeNode(new BookInfo
+            ("The Java Language Specification",
+             "jls.html"));
+        category.add(book);
+    }
+	
+	private class BookInfo {
+        public String bookName;
+        public URL bookURL;
+
+        public BookInfo(String book, String filename) {
+            bookName = book;
+            bookURL = getClass().getResource(filename);
+            if (bookURL == null) {
+                System.err.println("Couldn't find file: "
+                                   + filename);
+            }
+        }
+
+	}
 }
